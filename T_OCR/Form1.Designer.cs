1ee3ff7476326e280a810010f449bc4df3c7d519
jun <myjun1124@gmail.com>
2020-07-29 14:01:27 +0900

    초기 커밋.

---
+++ T_OCR/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace T_OCR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Button1 = new System.Windows.Forms.Button();
+            this.Button2 = new System.Windows.Forms.Button();
+            this.Button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Button1
+            // 
+            this.Button1.Location = new System.Drawing.Point(12, 12);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(100, 30);
+            this.Button1.TabIndex = 0;
+            this.Button1.Text = "파일 불러오기";
+            this.Button1.UseVisualStyleBackColor = true;
+            this.Button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Button2
+            // 
+            this.Button2.Location = new System.Drawing.Point(12, 182);
+            this.Button2.Name = "Button2";
+            this.Button2.Size = new System.Drawing.Size(100, 30);
+            this.Button2.TabIndex = 1;
+            this.Button2.Text = "전처리";
+            this.Button2.UseVisualStyleBackColor = true;
+            this.Button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Button3
+            // 
+            this.Button3.Location = new System.Drawing.Point(12, 295);
+            this.Button3.Name = "Button3";
+            this.Button3.Size = new System.Drawing.Size(100, 30);
+            this.Button3.TabIndex = 2;
+            this.Button3.Text = "인식";
+            this.Button3.UseVisualStyleBackColor = true;
+            this.Button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.Location = new System.Drawing.Point(182, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 200);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox2.Location = new System.Drawing.Point(488, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(300, 200);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(182, 225);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(300, 100);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(488, 225);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(300, 100);
+            this.textBox2.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Button3);
+            this.Controls.Add(this.Button2);
+            this.Controls.Add(this.Button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Button1;
+        private System.Windows.Forms.Button Button2;
+        private System.Windows.Forms.Button Button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ T_OCR/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Tesseract;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+
+namespace T_OCR
+{
+    public partial class Form1 : Form
+    {
+
+        Bitmap bmp1, bmp2;
+        Mat src1, src2;
+        
+        TesseractEngine engine = new TesseractEngine(@"./tessdata", "digits", EngineMode.Default);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            // Button1 클릭 이벤트
+            // 컴퓨터 내의 이미지를 pictureBox1에 띄워줌.
+
+            String imgfile = string.Empty;
+
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.InitialDirectory = "C:/";
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                imgfile = dialog.FileName;
+            }
+
+            try
+            {
+                bmp1 = new Bitmap(imgfile);
+
+                pictureBox1.Image = bmp1;
+                pictureBox2.Image = bmp1;
+            }
+            catch { }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            // Button2 클릭 이벤트
+            // 온도로 표시되는 특정 색상만을 추출하여 pictureBox1에 띄움.
+
+            // 색상 검출에 쓰이는 Opencv의 Mat 형식을 사용하기 위해 Bitmap 형식을 Mat으로 바꿔줌.
+            src1 = BitmapConverter.ToMat(bmp1);
+            src2 = BitmapConverter.ToMat(bmp1);
+            Mat[] mv1 = new Mat[3];
+            Mat[] mv2 = new Mat[3];
+            Mat mask1 = new Mat();
+            Mat mask2 = new Mat();
+
+            // 특정 색 검출을 위해 RBG 변환
+            Cv2.CvtColor(src1, src1, ColorConversionCodes.BGR2RGB);
+            mv1 = Cv2.Split(src1);
+            Cv2.CvtColor(src1, src1, ColorConversionCodes.RGB2BGR);
+
+            // 색상 추출 시 필요한 값을 기준으로 영역 내 색상을 나타내는 마스크 생성
+            // 최솟값 ~ 최댓값 사이의 색상(RGB는 3채널로 3채널 Scalar)을 이용해 mask 생성
+            Cv2.InRange(mv1[0], new Scalar(0, 240, 0), new Scalar(14, 255, 3), mask1);
+            // 원본 이미지에 BitwiseAnd 연산을 통해 원하는 색상 추출
+            Cv2.BitwiseAnd(src1, mask1.CvtColor(ColorConversionCodes.GRAY2BGR), src1);
+
+            // 적색은 BGR로 색 추출하는 것이 나음.
+            Cv2.CvtColor(src2, src2, ColorConversionCodes.BGR2BGR555);
+            mv2 = Cv2.Split(src2);
+            Cv2.CvtColor(src2, src2, ColorConversionCodes.BGR5552BGR);
+
+            Cv2.InRange(mv2[0], new Scalar(0, 0, 250), new Scalar(3, 3, 255), mask2);
+            // Cv2.InRange(mv2[0], new Scalar((double)Scalar.Red), new Scalar((double)Scalar.Red), mask2);
+            Cv2.BitwiseAnd(src2, mask2.CvtColor(ColorConversionCodes.GRAY2BGR), src2);
+
+            // 추출한 색을 바탕으로 이진화.
+            Mat gray1 = new Mat();
+            Mat gray2 = new Mat();
+
+            Cv2.CvtColor(src1, gray1, ColorConversionCodes.BGR2GRAY);
+            Cv2.Threshold(gray1, src1, 50, 240, ThresholdTypes.BinaryInv);
+
+
+            // Mat 형식은 pictureBox에 띄울 수 없으므로 다시 Bitmap 형식으로 변환
+            bmp1 = BitmapConverter.ToBitmap(src1);
+
+            pictureBox1.Image = bmp1;
+
+            Cv2.CvtColor(src2, gray2, ColorConversionCodes.BGR2GRAY);
+            Cv2.Threshold(gray2, src2, 50, 240, ThresholdTypes.BinaryInv);
+
+            bmp2 = BitmapConverter.ToBitmap(src2);
+
+            pictureBox2.Image = bmp2;
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            // Button4 클릭 이벤트
+            // Tesseract를 이용하여 문자 인식 후 textBox1에 출력
+            // LSTM으로 훈련된 학습 모델과 데이터를 사용
+
+            Pix pix1 = PixConverter.ToPix(bmp1);
+            Pix pix2 = PixConverter.ToPix(bmp2);
+
+            var result1 = engine.Process(pix1);
+            textBox1.Text = result1.GetText();
+            result1.Dispose();
+
+            var result2 = engine.Process(pix2);
+            textBox2.Text = result2.GetText();
+            result2.Dispose();
+
+            // MessageBox.Show(result.GetText());
+        }
+    }
+}
